f9c06f8558c95f91d44eed9fbd61fc72f7b81bea
Jovonne Cameron <70806828+curlygirltech@users.noreply.github.com>
2023-06-10 20:18:27 -0400

    first commit

---
+++ Javascript/app.js
@@ -0,0 +1,6 @@
+console.log("file connected");
+
+const update = document.getElementById("update");
+const deleteSugg = document.getElementById("delete");
+// console.log(update, deleteSugg)
+//add logic for form submission and apend it to the document
